5edd20c0fc35723850c6f8e0c49a733be727e5eb
Bojie Jia <u6730978@anu.edu.au>
2019-10-11 14:59:53 +1100

    button onclick demo

---
+++ app/src/main/java/com/example/sccl/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.sccl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void numberSeven(View view){
+        Button button=(Button)view;
+        String addContent=button.getText().toString();
+
+    }
+    //click seven, show "7" in the input text view
+    public void numberEight (View view){
+
+    }
+    public void numberNine(View view){
+
+    }
+    public void numberFour(View view){
+
+    }
+    public void numberFive(View view){
+
+    }
+    public void numberSix(View view){
+
+    }
+    public void numberOne(View view){
+
+    }
+    public void numberTwo(View view){
+
+    }
+    public void numberThree(View view){
+
+    }
+    public void numberZero(View view){
+
+    }
+    public void dot(View view){
+
+    }
+    public void equal(View view){
+
+    }
+    public void plus(View view){
+
+    }
+    public void minus(View view){
+
+    }
+    public void multipy(View view){
+
+    }
+    public void divide(View view){
+
+    }
+    public void delete(View view){}
+    public void allClear(View view){
+
+    }
+    public void variableX(View view){
+
+    }
+    public void variableY(View view){
+
+    }
+    public void function(View view){
+
+    }
+    public void sin(View view){
+
+    }
+    public void cos(View view){
+
+    }
+    public void tan(View view){
+
+    }
+    public void power(View view){
+
+    }
+    public void log(View view){
+
+    }
+    public void save(View view){
+
+    }
+    public void load(View view){
+
+    }
+    public void leftBracket(View view){
+
+    }
+    public void rightBracket(View view){
+
+    }
+    public void leftBrace(View view){
+
+    }
+    public void rightBrace(View view){
+
+    }
+    public void leftParentheses(View view){
+    }
+
+    public void rightParentheses(View view){
+
+    }
+
+
+
+
+}
